bf4cf19886994c268038f1812f7f86076fcf5e63
Sarwarhridoy4 <ssarwarhridoy4@gmail.com>
2023-07-18 13:32:16 +0600

    login and signup done

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const SECRET_KEY = "your-secret-key"; // Replace with your secret key
+const bcrypt = require("bcrypt");
 require("dotenv").config();
-
+const SECRET = process.env.SECRET_KEY; // Secre
+const URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9rpk71q.mongodb.net/house_rental_db?retryWrites=true&w=majority`
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9rpk71q.mongodb.net/?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose
+  .connect(
+    URL,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(console.log("Database Connected"))
+  .catch((err) => console.log("Error is:", err));
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
@@ -57,15 +61,14 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 app.post("/register", async (req, res) => {
   try {
     const { fullName, role, phoneNumber, email, password } = req.body;
     const user = new User({ fullName, role, phoneNumber, email, password });
     await user.save();
-    res.status(201).json({ message: "User registered successfully." });
+      res.status(201).json({ message: "User registered successfully." });
   } catch (err) {
     res.status(500).json({ error: "Error registering user." });
   }
@@ -86,7 +89,7 @@ app.post("/login", async (req, res) => {
       return res.status(401).json({ error: "Invalid credentials." });
     }
 
-    const token = jwt.sign({ id: user._id }, SECRET_KEY, { expiresIn: "1h" });
+    const token = jwt.sign({ id: user._id }, SECRET, { expiresIn: "1h" });
 
     res.json({ token });
   } catch (err) {
@@ -95,31 +98,30 @@ app.post("/login", async (req, res) => {
 });
 
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-  
-    if (!token) {
-      return res.status(401).json({ error: 'No token provided.' });
-    }
-  
-    jwt.verify(token, SECRET_KEY, (err, user) => {
-      if (err) {
-        return res.status(403).json({ error: 'Invalid token.' });
-      }
-      req.user = user;
-      next();
-    });
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "No token provided." });
   }
-  
-  // Protected route example
-  app.get('/protected', authenticateToken, (req, res) => {
-    res.json({ message: 'This is a protected route.' });
+
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: "Invalid token." });
+    }
+    req.user = user;
+    next();
   });
+}
 
+// Protected route example
+app.get("/protected", authenticateToken, (req, res) => {
+  res.json({ message: "This is a protected route." });
+});
 
-app.get('/', async (req, res) => {
-    res.send({message:`House Rent Server Running on port :${PORT}`})
-})
+app.get("/", async (req, res) => {
+  res.send({ message: `House Rent Server Running on port :${PORT}` });
+});
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
